49636cd63d7d31ec637c79e65e9f8e8d0d3daa9d
Christopher Garvis <cgarvis@gmail.com>
2013-03-08 11:11:27 -0500

    Sets up blog project

--- modules/projects/manifests/blog.pp
+++ modules/projects/manifests/blog.pp
@@ -1,14 +1,16 @@
 class projects::blog {
-  $home     = "/Users/${::luser}"
-  $projects = "${home}/projects"
-  $blog     = "${projects}/moveline.blog"
+  $home         = "/Users/${::luser}"
+  $projects     = "${home}/projects"
+  $projectDir   = "${projects}/moveline.blog"
 
   file { $projects:
     ensure  => 'directory'
   }
 
-  repository { $blog:
-    source  => 'Moveline/blog',
-    require => File[$projects]
+  boxen::project { 'blog':
+    dir     => $projectDir,
+    nginx   => 'projects/shared/nginx.jekyll.conf.erb',
+    ruby    => '1.9.3',
+    source  => 'Moveline/blog'
   }
 }
