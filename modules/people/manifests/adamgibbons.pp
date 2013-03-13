62a65d7d527d47592032037b18f5a2eaebf5e4c9
Adam Gibbons <adam@moveline.com>
2013-03-13 11:02:02 -0300

    Update adamgibbons.pp

--- modules/people/manifests/adamgibbons.pp
+++ modules/people/manifests/adamgibbons.pp
@@ -1,7 +1,10 @@
 class people::adamgibbons {
   include people::adamgibbons::applications
   include people::adamgibbons::dotfiles
+  include people::adamgibbons::preferences
 
+  # Projects
   include projects::blog
+  include projects::product
   include projects::rig
 }
