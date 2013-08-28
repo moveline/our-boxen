edcf99138e82983fb2d7be8e12ce1fecbccff632
Frederick Cook <cook.frederick@gmail.com>
2013-08-28 15:39:51 -0400

    added pp file for frederickcook

---
+++ modules/people/manifests/frederickcook.pp
@@ -0,0 +1,13 @@
+class people::russmatney {
+  include people::russmatney::preferences
+  include people::russmatney::applications
+  include people::russmatney::dotfiles
+
+  include projects::blog
+  include projects::ios
+  include projects::node-sendhub
+  include projects::product
+  include projects::trailer
+  include projects::tractor
+  include projects::shop
+}
