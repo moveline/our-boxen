473df18d49dd9506e866bad72b2b2471547d265c
Max Terry <mhterry@gmail.com>
2013-09-03 20:39:42 -0700

    Create file

---
+++ modules/people/manifests/maxwellterry.pp
@@ -0,0 +1,8 @@
+class people::maxwellterry {
+  include projects::blog
+  include projects::node-sendhub
+  include projects::product
+  include projects::shop
+  include projects::tractor
+  include projects::trailer
+}
