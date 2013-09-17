08d3d5e361e00de04de700679bd3e8bc60107966
Russell Matney <russell.matney@gmail.com>
2013-09-17 15:13:32 -0400

    specifies local goversion

--- modules/projects/manifests/speedometer.pp
+++ modules/projects/manifests/speedometer.pp
@@ -8,10 +8,6 @@ class projects::speedometer {
   $home        = "/Users/${::luser}"
   $projectsdir = "${home}/projects"
 
-  go::local { '/opt/boxen/goenv/shims/go/':
-    version => '1.1.1'
-  }
-
   boxen::project { 'speedometer':
     dir      => "${projectsdir}/go-projects/src/speedometer",
     source   => "moveline/speedometer"
