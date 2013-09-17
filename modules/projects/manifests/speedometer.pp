1ba0de9f842c6c780e16675819f32440b6e21aaf
Russell Matney <russell.matney@gmail.com>
2013-09-17 13:33:20 -0400

    updates speedometer manifest to specify the go version

--- modules/projects/manifests/speedometer.pp
+++ modules/projects/manifests/speedometer.pp
@@ -1,13 +1,13 @@
 class projects::speedometer {
   include projects::config
-
   include go
 
+  go::version { '1.1.1': }
   $home        = "/Users/${::luser}"
   $projectsdir = "${home}/projects"
 
   boxen::project { 'speedometer':
-	  dir      => "${projectsdir}/go-projects/src/speedometer",
+    dir      => "${projectsdir}/go-projects/src/speedometer",
     source   => "moveline/speedometer"
-	}	
+  }
 }
