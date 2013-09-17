7661659ec46ffd3e1ee5d3112f495e5b39d18b84
Russell Matney <russell.matney@gmail.com>
2013-09-17 15:32:56 -0400

    round 5....FIGHT

--- modules/projects/manifests/speedometer.pp
+++ modules/projects/manifests/speedometer.pp
@@ -1,12 +1,12 @@
 class projects::speedometer {
   include projects::config
-  include go::1_1
 
   $home        = "/Users/${::luser}"
   $projectsdir = "${home}/projects"
 
   boxen::project { 'speedometer':
     dir      => "${projectsdir}/go-projects/src/speedometer",
+    go       => '1.1.1'
     source   => "moveline/speedometer"
   }
 }
