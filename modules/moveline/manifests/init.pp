b4a55b22b2984581c1c1ea54e66d73a80611a186
Russell Matney <russell.matney@gmail.com>
2013-09-16 20:42:40 -0400

    clean up

--- modules/moveline/manifests/init.pp
+++ modules/moveline/manifests/init.pp
@@ -7,11 +7,6 @@ class moveline {
   include tmux
   include wget
   include zsh
-  include go
-
-  go::local { "${boxen::config::home}/projects/go-projects":
-    version => '1.1.1'
-  }
 
   class { 'ruby::global':
     version => '2.0.0'
