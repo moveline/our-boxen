746ef0c0807b2ddd861365612608c57c45d9a512
Crystal Chang <crystalschang@gmail.com>
2013-10-07 12:35:54 -0700

    Adding crystalchang.pp

---
+++ modules/people/manifests/crystalchang.pp
@@ -0,0 +1,11 @@
+class people::crystalchang {
+  # Projects
+  include projects::blog
+  include projects::product
+  include projects::node-sendhub
+  include projects::shop
+  include projects::speedometer
+  include projects::trailer
+  include projects::tractor
+  include projects::logbook
+}
